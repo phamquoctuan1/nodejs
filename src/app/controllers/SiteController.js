af6a5c497e24792b9850dc6ceebcf311531ea584
phamquoctuan <72914569+phamquoctuan1@users.noreply.github.com>
2021-06-04 09:54:21 +0700

    create models Course connect to DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,12 +1,22 @@
+const Course = require("../models/Course");
+
 class SiteController {
-    //[GET] /news
-    index(req, res) {
-        res.render('home');
-    }
-    //[GET] /news/:slug
-    search(req, res) {
-        res.render('search');
-    }
+  //[GET] /news
+  index(req, res) {
+    Course.find({}, function (err, courses) {
+      if (!err) {
+        res.json(courses);
+        return;
+      }else {
+        res.status(400).json({ error: "Loi" });
+      }
+    });
+    // res.render('home');
+  }
+  //[GET] /news/:slug
+  search(req, res) {
+    res.render("search");
+  }
 }
 
 module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -5,32 +5,41 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+
 const route = require('./routes');
+const db = require('./config/db');
+
+
+// Connect to DB
+
+db.connect();
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-    express.urlencoded({
-        extended: true,
-    }),
+  express.urlencoded({
+    extended: true,
+  })
 );
-app.use(   express.json());
+app.use(express.json());
 //Http Logger
 //app.use(morgan('combined'))
 
 //Template Engine
-     app.engine(
-    'hbs',
-    handlebars({
-        extname: '.hbs',
-    }),
+app.engine(
+  'hbs',
+  handlebars({
+    extname: '.hbs',
+  })
 );
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources','views'));
 
 // routes init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
